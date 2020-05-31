e76b4cc3077794aebd6f430f5b0a2aaf17dd7a9e
Ebenezer Don <ebenezerdonu@gmail.com>
2020-05-31 01:50:43 +0100

    add framer motion and css keyframe animation

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import { CSSTransitionGroup } from 'react-transition-group'
+import { motion } from "framer-motion"
 
 const FirstPage = ({ setHomepage }) => {
   return (
     <div className="first-page">
-      <CSSTransitionGroup
-        transitionName="fadeIn"
-        transitionAppear={true}
+      <motion.p
+        className="title"
+        initial={{opacity: 0, y: 100}}
+        animate={{ opacity: [0, 1], y: [100, 100, 0]}}
+        transition={{ ease: "easeOut", duration: 1 }}
       >
-        <p className="title">
-          Small changes can make a big impact on our planet
-        </p>
-      </CSSTransitionGroup>
-      {/* <CSSTransitionGroup> */}
-        <p className="sub-text">
-          We all want a healthy planet for today and tomorrow.
-          The small choices we make each day can help us get
-          there. Learn simple tips from Google, the California
-          Academy of Sciences, and the Ellen MacArthur Foundation
-          to help people and the planet thrive together.
-        </p>
-      {/* </CSSTransitionGroup> */}
-      {/* <CSSTransitionGroup> */}
-        <button className="button blue"
-          onClick={() => setHomepage(false)}>
-            Let's get started
-        </button>
-      {/* </CSSTransitionGroup> */}
+        Small changes can make a big impact on our planet
+      </motion.p>
+      <motion.p
+        className="sub-text"
+        initial={{opacity: 0, y: 180}}
+        animate={{opacity: [0, 0, 0, 1], y: [180, 180, 0]}}
+        transition={{ ease: "easeOut", duration: 1.3 }}
+      >
+        We all want a healthy planet for today and tomorrow.
+        The small choices we make each day can help us get
+        there. Learn simple tips from Google, the California
+        Academy of Sciences, and the Ellen MacArthur Foundation
+        to help people and the planet thrive together.
+      </motion.p>
+      <motion.button
+        className="button blue"
+        onClick={() => setHomepage(false)}
+        initial={{opacity: 0, y: 180}}
+        animate={{opacity: [0, 0, 0, 1], y: [180, 150, 150, 0]}}
+        transition={{ ease: "easeOut", duration: 1.3 }}
+      >
+          Let's get started
+      </motion.button>
     </div>
   )
 }
--- src/SecondPage.js
+++ src/SecondPage.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const SecondPage = ({ setHomepage }) => {
   return (
-    <div class="second-page">
-      <button className="button" onClick={() => setHomepage(true)}>Back</button>
-      <p classsName="title">Your Plan,</p>
-      <p className="title">&nbsp; &nbsp; Your Planet</p>
+    <div className="second-page">
+      <button className="button" onClick={() => setHomepage(true)}>
+        Home
+      </button>
+      <motion.h1
+        className="title"
+        initial={{x: 100, y: -100}}
+        animate={{x: 0, y: 0}}
+        transition={{ ease: "easeOut", duration: 1.3 }}
+      >
+        Your Plan, &nbsp; &nbsp; <br/> &nbsp; &nbsp; &nbsp; Your Planet
+      </motion.h1>
+      <motion.div
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+      >
+        <div className="outer circle1" />
+        <div className="outer circle2" />
+        <div className="outer circle3" />
+        <div className="outer circle4"/>
+        <div className="inner circle1" />
+        <div className="inner circle2" />
+        <div className="inner circle3" />
+        <div className="inner circle4" />
+      </motion.div>
     </div>
   )
 }

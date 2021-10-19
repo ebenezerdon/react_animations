e624232f3c44abacf4887463a7ea19bef8a6aa5a
ebenezerdon <ebenezerdonu@gmail.com>
2021-10-19 16:11:00 +0100

    update branch to starter code

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,26 +1,17 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
 
 const GetStarted = () => {
   return (
     <div className="getStarted">
-      <motion.h1
-        className="title"
-        initial={{ x: 100, y: -100 }}
-        animate={{ x: 0, y: 0 }}
-        transition={{ ease: 'easeOut', duration: 1.3 }}
-        drag={true}
-        dragConstraints={{ top: 1, bottom: 1, right: 1, left: 1 }}
-        dragElastic={1}
-        whileHover={{cursor: "pointer"}}
-      >
-        Your Plan, &nbsp; &nbsp; <br/> &nbsp; &nbsp; &nbsp; Your Planet
-      </motion.h1>
+      <h1 className="title">
+        Your Plan, &nbsp; &nbsp; <br/>
+        &nbsp; &nbsp; &nbsp; Your Planet
+      </h1>
 
       <Link to='/' className="button">Home</Link>
 
-      <motion.div animate={{opacity: [0, 1]}}>
+      <div>
         <div className="circle c1" />
         <div className="circle c1 ripple" />
         <div className="circle c2" />
@@ -29,7 +20,7 @@ const GetStarted = () => {
         <div className="circle c3 ripple" />
         <div className="circle c4" />
         <div className="circle c4 ripple"/>
-      </motion.div>
+      </div>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 import { useHistory } from 'react-router-dom'
 
 const Home = () => {
@@ -7,35 +6,22 @@ const Home = () => {
 
   return (
     <div className="home">
-      <motion.p
-        className="title"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: [0, 1], y: [100, 100, 0] }}
-        transition={{ ease: 'easeOut', duration: 1 }}
-      >
+      <p className="title">
         Small changes can make a big impact on our planet
-      </motion.p>
-      <motion.p
-        className="sub-text"
-        initial={{opacity: 0, y: 180}}
-        animate={{opacity: [0, 0, 0, 1], y: [180, 180, 0]}}
-        transition={{ ease: "easeOut", duration: 1.3 }}
-      >
+      </p>
+      <p className="sub-text">
         We all want a healthy planet for today and tomorrow.
         The small choices we make each day can help us get
         there. Learn simple tips from Google, the California
         Academy of Sciences, and the Ellen MacArthur Foundation
         to help people and the planet thrive together.
-      </motion.p>
-      <motion.button
+      </p>
+      <button
         className="button"
         onClick={() => history.push('/get-started')}
-        initial={{opacity: 0, y: 180}}
-        animate={{opacity: [0, 0, 0, 1], y: [180, 150, 150, 0]}}
-        transition={{ ease: "easeOut", duration: 1.3 }}
       >
           Let's get started
-      </motion.button>
+      </button>
     </div>
   )
 }
